eed7f3e2618a950dc7d60fc6cc5b90620a95fd1c
nazmuldev23 <nazmulhossen23.bd@gmail.com>
2026-04-24 10:41:52 +0600

    home screen

---
+++ lib/views/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  static const String name = '/home';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Home', style: TextStyle(fontSize: 16),),
+          ],
+        ),
+      ),
+    );
+  }
+}
